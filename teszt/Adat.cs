8247847b3da70e20f57b07c53a9027fc930efd46
Kölcze Szabolcs <kolcze.szabolc2007@gmail.com>
2026-01-30 08:41:51 +0100

    Valami hozzáadva az Adat osztályhoz

--- teszt/Adat.cs
+++ teszt/Adat.cs
@@ -10,15 +10,18 @@ namespace teszt
 	{
 		private string nev;
 		private int ar;
+		private double valami;
 
-		public Adat(string nev, int ar)
+		public Adat(string nev, int ar, double valami)
 		{
 			this.nev = nev;
 			this.ar = ar;
+			this.valami = valami;
 		}
 
 		public string Nev { get => nev; set => nev = value; }
 		public int Ar { get => ar; set => ar = value; }
+		public double Valami { get => valami; set => valami = value; }
 
 		public override string ToString()
 		{
